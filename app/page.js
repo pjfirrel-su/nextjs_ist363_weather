d7f1d187044f0e6157518dbc29bbf36842ea37a4
pjfirrel-su <123100210+pjfirrel-su@users.noreply.github.com>
2024-04-08 19:52:01 -0400

    environment variable added

--- app/page.js
+++ app/page.js
@@ -8,30 +8,52 @@ import ButtonDemo from "../components/ButtonDemo";
 import ColorPicker from "../components/ColorPicker";
 import PeoplePicker from "../components/PeoplePicker";
 
-import { getPeople, getWeatherData } from "../lib/api";
+import { getGeoLocation, getPeople, getWeatherData, getWeatherDataByLatLon } from "../lib/api";
 
 
 const Homepage = () => {
   const [weatherData, setWeatherData] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
   const peopleArr = getPeople();
 
   useEffect(() => {
+    getGeoLocation().then((position) => {
+      // console.log(position);
+      setLocation(position);
+    })
+    .catch((error) => {
+      // console.log(error);
+      setErrorMsg(error);
+    });
+  }, []);
+
+  useEffect(() => {
     const fetchData = async () => {
-      const response = await getWeatherData();
+      const response = await getWeatherDataByLatLon(location);
       setWeatherData(response);
     };
-    fetchData();
-  }, []);
+    location ? fetchData() : null;
+  }, [location]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const response = await getWeatherData();
+  //     setWeatherData(response);
+  //   };
+  //   fetchData();
+  // }, []);
 
   // console.log ({ peopleArr });
 
   return (
   <div>
     <h1>Weather App</h1>
+    {errorMsg && <div>{errorMsg}</div>}
     {weatherData && (
       <div>
-        <h2>weatherData.city.name</h2>
+        <h2>{weatherData.city.name}</h2>
         <p>Current Temp: {weatherData.list[0].main.temp}&deg; F</p>
         <p>{weatherData.list[0].weather[0].description}</p>
         <Image 
